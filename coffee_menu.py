7283a6a441056aa92471b40b42367af3e27298e4
fafafoyi <fatihefeunal236@gmail.com>
2025-10-15 11:59:33 +0200

    Coffee menu finished

--- coffee_menu.py
+++ coffee_menu.py
@@ -1,4 +1,6 @@
 import sqlite3
+from unittest import expectedFailure
+
 
 class coffee_menu:
 
@@ -16,4 +18,39 @@ class coffee_menu:
             cursor.execute("SELECT name, price FROM coffee_types ORDER BY price ASC")
             items = []
             for name, price in cursor.fetchall():
-                items.append({})
+                items.append({"name": name, "price": price})
+            return items
+        except sqlite3.Error as e:
+            print(f"Database Error: {e}" )
+            return []
+        finally:
+            if conn:
+                conn.close()
+
+    def display_menu(self):
+
+        if not self.menu_items:
+            print("Menu is currently unavailable. Please Check the Database")
+            return
+        print("Welcome to OOP Coffee Terminal")
+        print("-" * 35)
+        print(f"| {'Item':<20} | {'Price':>8} |")
+        print("-" * 35)
+
+        for item in self.menu_items:
+            price_str = f"{item["price"]:.2f}:"
+            print(f"{item["name"]:<20} | {price_str:>8} |" )
+        print("-" * 35)
+
+        choices = [item["name"].lower() for item in self.menu_items]
+        return choices
+
+    def find_item(self, order_name):
+
+        order_name = order_name.lower()
+        for item in self.menu_items:
+            if item["name"].lower() == order_name:
+                return item
+        return None
+
+
